d971b9174eb0a527fff7d30f13ff216d6576858d
Oleh Topchak <dgtop997@gmail.com>
2020-02-22 17:10:09 +0200

    HW-04 Created test fot search pipe

--- src/app/modules/courses/pipes/search.pipe.spec.ts
+++ src/app/modules/courses/pipes/search.pipe.spec.ts
@@ -1,8 +1,31 @@
 import { SearchPipe } from './search.pipe';
+import { MOCK_DATA } from 'assets/mock/db';
+
 
 describe('SearchPipe', () => {
+  let search: SearchPipe;
+  beforeEach(() => {
+    search = new SearchPipe();
+  });
   it('create an instance', () => {
-    const pipe = new SearchPipe();
-    expect(pipe).toBeTruthy();
+    expect(search).toBeTruthy();
+  });
+
+  it('should filter courses by name', () => {
+    const coursesLength = MOCK_DATA.length;
+    const coursesLeft = search.transform(MOCK_DATA, 'java').length;
+    const isLessCourses: boolean = coursesLeft < coursesLength;
+    expect(isLessCourses).toBeTruthy();
+  });
+
+  it('should left only courses included "java" in name', () => {
+    const coursesLeft = search.transform(MOCK_DATA, 'java');
+    expect(coursesLeft.length).toBe(3);
+  });
+
+  it('should show all courses if name is empty', () => {
+    const coursesLength = MOCK_DATA.length;
+    const coursesLeft = search.transform(MOCK_DATA, '');
+    expect(coursesLeft.length).toBe(coursesLength);
   });
 });
